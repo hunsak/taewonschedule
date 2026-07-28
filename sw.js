62fd84bc99a7f1301938c5e4df22ff5b67e6579f
hunsak <67245964+hunsak@users.noreply.github.com>
2026-07-29 07:58:05 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-
-const CACHE_NAME = 'taewon-schedule-v1';
+const CACHE_NAME = 'taewon-schedule-v2';
 const CORE_ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
@@ -19,8 +18,10 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+  // 항상 브라우저 HTTP 캐시를 건너뛰고 진짜 네트워크로 다시 물어봐서,
+  // 다른 기기에서 방금 바꾼 최신 내용을 곧바로 받아오도록 함
   event.respondWith(
-    fetch(event.request).then((res) => {
+    fetch(event.request, { cache: 'no-store' }).then((res) => {
       const resClone = res.clone();
       caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
       return res;
